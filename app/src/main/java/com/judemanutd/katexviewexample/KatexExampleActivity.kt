fbf6932f597a916291a5c03c4871f4588037c2dc
Jude <jude.manutd@gmail.com>
2019-03-14 23:23:12 +0530

    initial commit

---
+++ app/src/main/java/com/judemanutd/katexviewexample/KatexExampleActivity.kt
@@ -0,0 +1,12 @@
+package com.judemanutd.katexviewexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KatexExampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_katex_example)
+    }
+}
