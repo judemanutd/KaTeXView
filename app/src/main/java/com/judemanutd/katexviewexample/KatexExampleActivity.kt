c8bf6e92357f086869145af0b865293d1340942b
Jude Fernandes <jude.manutd@gmail.com>
2020-02-16 13:08:32 +0530

    fixed issue with text color - #3, #4 updated dependencies

--- app/src/main/java/com/judemanutd/katexviewexample/KatexExampleActivity.kt
+++ app/src/main/java/com/judemanutd/katexviewexample/KatexExampleActivity.kt
@@ -1,5 +1,6 @@
 package com.judemanutd.katexviewexample
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_katex_example.*
@@ -13,5 +14,8 @@ class KatexExampleActivity : AppCompatActivity() {
         val text = "$$ c = \\pm\\sqrt{a^2 + b^2} $$"
 
         katex_text.setText(text)
+
+        katex_text_2.setText(text)
+        katex_text_2.setTextColor(Color.BLUE)
     }
 }
